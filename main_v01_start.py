221eef77c4ca86317202bdb28d813afa4d4e94f7
tonynoce <95554128+tonynoce@users.noreply.github.com>
2022-07-06 14:46:53 -0300

    Added a "start with"

---
+++ main_v01_start.py
@@ -0,0 +1,64 @@
+from os import remove, walk, listdir, scandir
+
+
+"""
+script to delete duplicate .extension files
+"""
+
+def popLastItem(list):
+    list.pop()
+    return list
+
+def filesToDelete(extension):
+    filesToDelete = [] 
+
+    # file scanning
+    for files in scandir(workingDir):
+        if files.name.startswith(extension) and files.is_file():
+            #print(files)
+            filesToDelete.append(files)
+    print("Files detected: \n", filesToDelete)
+    popLastItem(filesToDelete)
+    
+    print("Files to delete: \n", filesToDelete)
+    return filesToDelete
+
+
+def removeFiles():
+    #global filesToDelete
+    # remove section
+    for file in filesToDelete:
+        remove(file)
+        print("The file", file, "has been deleted.")
+
+def listDir():
+    for files in listdir(workingDir):
+        print(files)
+
+# set working dir
+workingDir = input("Working dir: ")
+extension = input("Define Extension: ")
+
+# filesToDelete()
+filesToDelete = filesToDelete(extension)
+
+# ask user to continue
+while True:
+    try:
+        opcion = input("(Y) to continue, (N) to cancel: ")
+        if opcion == "Y":
+
+        # remove files function
+            removeFiles()
+            #print("\nThe directory now has this files:\n")
+            #listDir()
+            print("\nThank you for using this app !\n")
+            break
+
+        elif opcion == "N":
+            break
+
+    except Exception as e:
+        continue 
+
+
